b0fa3d221dfcd693ee41ae6bc9c60242542e8725
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-02-08 18:53:02 +0900

    fix unix console behavior

--- inputimeout/inputimeout.py
+++ inputimeout/inputimeout.py
@@ -1,8 +1,8 @@
 import sys
 import platform
+import time
 
 if platform.system() == 'Windows':
-    import time
     import msvcrt
 else:
     import selectors
@@ -15,6 +15,7 @@ CR = '\r'
 LF = '\n'
 CRLF = CR + LF
 
+
 class TimeoutOccurred(Exception):
     pass
 
@@ -24,15 +25,38 @@ def echo(string):
     sys.stdout.flush()
 
 
-def inputimeout(prompt='', timeout=DEFAULT_TIMEOUT):
-    echo(prompt)
+# def getch():
+#     sel = selectors.DefaultSelector()
+#     sel.register(sys.stdin, selectors.EVENT_READ)
+#     events = sel.select(INTERVAL)
+#
+#     if events:
+#         key, _ = events[0]
+#         return key.fileobj.read(1)
+#     else:
+#         raise TimeoutOccurred
+#
+#
+# def read_char():
+#     fd = sys.stdin.fileno()
+#     old_settings = termios.tcgetattr(fd)
+#     try:
+#         tty.setraw(sys.stdin.fileno())
+#         ch = sys.stdin.read(1)
+#     finally:
+#         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+#     return ch
 
+
+def inputimeout(prompt='', timeout=DEFAULT_TIMEOUT):
     if platform.system() == 'Windows':
         return win_inputimeout(prompt, timeout)
     else:
         return posix_inputimeout(prompt, timeout)
 
+
 def posix_inputimeout(prompt='', timeout=DEFAULT_TIMEOUT):
+    echo(prompt)
     sel = selectors.DefaultSelector()
     sel.register(sys.stdin, selectors.EVENT_READ)
     events = sel.select(timeout)
@@ -44,7 +68,9 @@ def posix_inputimeout(prompt='', timeout=DEFAULT_TIMEOUT):
         echo(LF)
         raise TimeoutOccurred
 
+
 def win_inputimeout(prompt='', timeout=DEFAULT_TIMEOUT):
+    echo(prompt)
     begin = time.monotonic()
     end = begin + timeout
     line = ''
