de3fddda75b03e74ea33f219bdb23afb8d9a73b8
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-02-08 19:50:33 +0900

    update API

--- inputimeout/__init__.py
+++ inputimeout/__init__.py
@@ -1,4 +1,4 @@
-from .inputimeout import inputimeout, TimeoutOccurred
+from .inputimeout import inputimeout, posix_inputimeout, win_inputimeout, TimeoutOccurred
 from .__version__ import (
     __version__, __author__, __author_email__, __copyright__, __license__,
     __description__, __name__, __url__,
