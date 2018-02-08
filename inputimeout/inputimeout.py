44ff69c2363b03a9cb99ad169f6eac4ac672ab83
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-02-08 14:39:57 +0900

    v1.0.0

--- inputimeout/inputimeout.py
+++ inputimeout/inputimeout.py
@@ -6,8 +6,9 @@ DEFAULT_TIMEOUT = 30.0
 CRLF = '\r\n'
 LF = '\n'
 
-
 if platform.system() == 'Windows':
+    import time
+    import msvcrt
     LINE_FEED_CODE = CRLF
 else:
     LINE_FEED_CODE = LF
@@ -25,6 +26,10 @@ def echo(string):
 def inputimeout(prompt='', timeout=DEFAULT_TIMEOUT):
 
     echo(prompt)
+
+    if LINE_FEED_CODE == CRLF:
+        return win_inputimeout(prompt, timeout)
+
     sel = selectors.DefaultSelector()
     sel.register(sys.stdin, selectors.EVENT_READ)
     events = sel.select(timeout)
@@ -36,3 +41,32 @@ def inputimeout(prompt='', timeout=DEFAULT_TIMEOUT):
     else:
         echo(LINE_FEED_CODE)
         raise TimeoutOccurred
+
+
+def put_str(string):
+    for c in string:
+        msvcrt.putwch(c)
+
+
+def win_inputimeout(prompt='', timeout=DEFAULT_TIMEOUT):
+    begin = time.monotonic()
+    end = begin + timeout
+    put_str(prompt)
+    line = ''
+
+    while time.monotonic() < end:
+        if msvcrt.kbhit():
+            c = msvcrt.getwch()
+            if c == '\r' or c == '\n':
+                put_str('\r\n')
+                return line
+            if c == '\003':
+                raise KeyboardInterrupt
+            if c == '\b':
+                line = line[:-1]
+                put_str('\r\n' + prompt + line)
+            else:
+                msvcrt.putwch(c)
+                line = line + c
+        time.sleep(0.05)
+    raise TimeoutOccurred
